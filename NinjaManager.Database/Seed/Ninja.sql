17d5f480b7efe5c9bfa3ad3fbc55bc04bf35ccff
BrandondeJongh <hbjongh@avans.nl>
2019-10-23 21:30:47 +0200

    Create Ninja.sql
    Ninja seed script

---
+++ NinjaManager.Database/Seed/Ninja.sql
@@ -0,0 +1,17 @@
+﻿SET IDENTITY_INSERT Ninja ON
+
+MERGE INTO dbo.Ninja AS Target  
+USING (values 
+	(1, 'Mik Rijer', 1000 ),
+	(2, 'Ser Garis', 1000),
+	(3, 'Sartijn Muurmans', 1000)
+) AS Source (Id, Name, Gold)  
+ON Target.Id = Source.Id  
+WHEN NOT MATCHED BY TARGET THEN  
+	INSERT (Id, Name, Gold)  
+	VALUES (Id, Name, Gold)  
+WHEN MATCHED THEN
+	UPDATE SET
+		Name = Source.Name;
+
+SET IDENTITY_INSERT Ninja OFF
