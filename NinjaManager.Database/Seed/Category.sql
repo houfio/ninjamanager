18dadde99f2279fea625efb61486187d93264b3a
BrandondeJongh <hbjongh@avans.nl>
2019-10-23 21:48:56 +0200

    Category + Item seed
    Category + Item seed

---
+++ NinjaManager.Database/Seed/Category.sql
@@ -0,0 +1,15 @@
+﻿SET IDENTITY_INSERT Category ON
+
+MERGE INTO dbo.Category AS Target  
+USING (values 
+	(1, 'Weapon'),
+	(2, 'Armor')
+) AS Source (Id, Name)  
+ON Target.Id = Source.Id  
+WHEN NOT MATCHED BY TARGET THEN  
+	INSERT (Id, Name)  
+	VALUES (Id, Name)  
+WHEN MATCHED THEN
+	UPDATE SET
+		Name = Source.Name;
+SET IDENTITY_INSERT Ninja OFF
---
+++ NinjaManager.Database/Seed/Item.sql
@@ -0,0 +1,19 @@
+﻿SET IDENTITY_INSERT Item ON
+
+MERGE INTO dbo.Item AS Target  
+USING (values 
+	(1, 'Sword', 300, 1),
+	(2, 'Chestplate', 500, 2),
+	(3, 'Pants', 200, 3)
+) AS Source (Id, Name, Price, CategoryId)  
+ON Target.Id = Source.Id  
+WHEN NOT MATCHED BY TARGET THEN  
+	INSERT (Id, Name, Price, CategoryId)  
+	VALUES (Id, Name, Price, CategoryId)  
+WHEN MATCHED THEN
+	UPDATE SET
+		Name = Source.Name,
+		Price = Source.Price,
+		CategoryId = Source.CategoryId;
+
+SET IDENTITY_INSERT Ninja OFF
