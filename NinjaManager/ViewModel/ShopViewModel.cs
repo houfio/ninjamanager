726ca2c36c4db33d5742088c995d82cfe1e2679b
houfio <26674115+houfio@users.noreply.github.com>
2019-10-26 16:59:41 +0200

    Add shop views

--- NinjaManager/ViewModel/ShopViewModel.cs
+++ NinjaManager/ViewModel/ShopViewModel.cs
@@ -1,6 +1,10 @@
-﻿using NinjaManager.Domain;
+﻿using GalaSoft.MvvmLight.Command;
+using NinjaManager.Domain;
+using NinjaManager.Model;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows;
+using System.Windows.Input;
 
 namespace NinjaManager.ViewModel
 {
@@ -8,15 +12,49 @@ namespace NinjaManager.ViewModel
     {
         public NinjaListModel List { get; }
         public Collection<string> Categories { get; }
+        public Visibility EquipmentVisiblity => Equipment.Count > 0 ? Visibility.Visible : Visibility.Hidden;
+        public Visibility DetailVisiblity => Selected != null ? Visibility.Visible : Visibility.Hidden;
+        public Collection<EquipmentModel> Equipment { get; }
+        public EquipmentModel Selected { get => _selected == -1 || _selected >= Equipment.Count ? null : Equipment[_selected]; set => Set(ref _selected, Equipment.IndexOf(value)); }
+        public ICommand CategoryCommand { get; }
+        public ICommand EquipmentCommand { get; }
+
+        private int _selected;
 
         public ShopViewModel(NinjaListModel list)
         {
             List = list;
+            Equipment = new ObservableCollection<EquipmentModel>();
+            CategoryCommand = new RelayCommand<string>(SelectCategory);
+            EquipmentCommand = new RelayCommand<EquipmentModel>(SelectEquipment);
 
             using (var entities = new NinjaManagerEntities())
             {
                 Categories = new ObservableCollection<string>(entities.Categories.OrderBy((c) => c.Order).Select((c) => c.Name).ToList());
             }
+        }
+
+        private void SelectCategory(string category)
+        {
+            using (var entities = new NinjaManagerEntities())
+            {
+                var list = entities.Equipments.Where((e) => e.Category == category).ToList();
+
+                Equipment.Clear();
+
+                foreach (var equipment in list)
+                {
+                    Equipment.Add(EquipmentModel.FromRaw(equipment));
+                }
+            }
+
+            RaisePropertyChanged(nameof(EquipmentVisiblity));
+        }
+
+        private void SelectEquipment(EquipmentModel equipment)
+        {
+            Selected = equipment;
+            RaisePropertyChanged(nameof(DetailVisiblity));
         }
     }
 }
